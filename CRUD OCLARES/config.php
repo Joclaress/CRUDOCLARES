441979165e87997ba06a033f441594d3711ddc72
hamagka023 <56012639+hamagka023@users.noreply.github.com>
2021-02-12 02:07:35 +0800

    Add files via upload

---
+++ CRUD OCLARES/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$databaseHost = 'localhost';
+$databaseName = 'crudjericho';
+$databaseUsername = 'root';
+$databasePassword = 'XQ4t5AUtAhjXSoX7';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
